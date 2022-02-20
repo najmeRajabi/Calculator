e62e0c1eef1216c6b5adf556ddbe84a7ab7a869b
niha <niha8515@yahoo.com>
2022-02-20 03:55:58 -0800

    create Calculate.kt and fun +,-,/,*

---
+++ app/src/main/java/com/example/calculator/Calculate.kt
@@ -0,0 +1,61 @@
+package com.example.calculator
+
+class Calculator(private var memory :Double = 0.0){
+
+
+    fun add(input1:Double , input2:Double):String{
+        val result = input1 + input2
+        memory = result
+        return result.toString()
+    }
+    fun add(input1:Double ): String {
+        val result = input1 + memory
+        memory = result
+        return result.toString()
+    }
+    fun subtraction(input1:Double , input2:Double):String{
+        val result = input1 - input2
+        memory = result
+        return result.toString()
+    }
+    fun subtraction(input1:Double ):String{
+        val result =  memory - input1
+        memory = result
+        return result.toString()
+    }
+    fun multiplication(input1:Double , input2:Double):String{
+        val result = input1 * input2
+        memory = result
+        return result.toString()
+    }
+    fun multiplication(input1:Double ):String{
+        val result = input1 * memory
+        memory = result
+        return result.toString()
+    }
+    fun division(input1:Double , input2:Double):String{
+        var result = 0.0
+        if (input2 == 0.0){
+            println(" division by zero not possible ")
+        }
+        else {
+            result = input1 / input2
+            memory = result
+        }
+        return result.toString()
+    }
+    fun division(input1:Double ):String{
+        var result = 0.0
+        if (input1 == 0.0){
+            println(" division by zero not possible ")
+        }
+        else {
+            result =  memory / input1
+            memory = result
+        }
+        return result.toString()
+    }
+    fun setMemory(Memory:Double){
+        memory = Memory
+    }
+}
