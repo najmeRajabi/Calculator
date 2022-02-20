fb9107883f84fca3ad4aa4fcf9425b5f04d868ab
niha <niha8515@yahoo.com>
2022-02-20 03:45:17 -0800

    create project and design

---
+++ app/src/main/java/com/example/calculator/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.example.calculator
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.calculator.databinding.ActivityMainBinding
+
+lateinit var binding: ActivityMainBinding
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+
+    }
+}
